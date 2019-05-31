cb4f42370656e2c2a0a2c00cc3560063ed21941e
anony111 <anony11@163.com>
2019-05-31 14:11:48 +0800

    add swagger2 config

---
+++ src/main/java/com/unicom/gitgithub/config/Swagger2Config.java
@@ -0,0 +1,9 @@
+package com.unicom.gitgithub.config;
+
+/**
+ * @ClassName:com.unicom.gitgithub.config
+ * @Author:Administrator Date:2019/5/31 0031
+ * Discription:
+ */
+public class Swagger2Config {
+}
